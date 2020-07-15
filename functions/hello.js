1f6816bd082da8ca5461f3d1f0da9f48a2f466ba
s3rj <astralninja@gmail.com>
2020-07-15 16:17:31 -0700

    add debug flags

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,7 @@
 // functions/hello.js
 const express = require('express');
 console.log(process.env.FIREBASE_MESSAGING_SENDER_ID);
+console.log('is this visible');
 exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
   return {
