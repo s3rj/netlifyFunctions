9a7359f3885946b10a47d5ab9c009f964671c70b
s3rj <astralninja@gmail.com>
2020-07-27 16:12:06 -0700

    serverless refactor

--- functions/twilioHandler.js
+++ functions/twilioHandler.js
@@ -9,34 +9,25 @@ const { urlencoded } = require('body-parser');
 const firebase = require('firebase');
 
 //firebase connection
-var config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+
 //fb snapshot
-database.ref().on("value", function(snapshot) {
+// database.ref().on("value", function(snapshot) {
 
-    // Then we console.log the value of snapshot
-  console.log(snapshot.val());
+//     // Then we console.log the value of snapshot
+//   console.log(snapshot.val());
 
-    // Then we change the html associated with the number.
-  //$("#click-value").text(snapshot.val().clickCount);
+//     // Then we change the html associated with the number.
+//   //$("#click-value").text(snapshot.val().clickCount);
 
-    // Then update the clickCounter variable with data from the database.
-  var clickCounter = snapshot.val().clickCount;
+//     // Then update the clickCounter variable with data from the database.
+//   var clickCounter = snapshot.val().clickCount;
 
-    // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-    }, function(errorObject) {
+//     // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
+//     }, function(errorObject) {
 
-    // In case of error this will print the error
-  console.log("The read failed: " + errorObject.code);
-});
+//     // In case of error this will print the error
+//   console.log("The read failed: " + errorObject.code);
+// });
 //
 
 
@@ -50,6 +41,16 @@ router.post('/sms', (request, response) => {
   console.log(
     `Incoming message from ${request.body.From}: ${request.body.Body}`
   );
+  var config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
   
 
   database.ref().set({
