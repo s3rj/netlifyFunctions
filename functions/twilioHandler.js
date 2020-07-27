bf7aa99700625a00f97ea560c23ef00b1a52398b
Sergey Sambros <astralninja@gmail.com>
2020-07-27 13:01:00 -0700

    Update twilioHandler.js

--- functions/twilioHandler.js
+++ functions/twilioHandler.js
@@ -75,6 +75,13 @@ app.post('/status', request => {
   //console.log('Did you uncomment the log statements above me?');
 });
 
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+
 app.use('/.netlify/functions/twilioHandler', router);  // path must route to lambda
 
 
