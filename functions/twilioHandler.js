643bcb696142e5a6f50b5b77239b6181c82c2344
s3rj <astralninja@gmail.com>
2020-07-27 16:36:54 -0700

    serverless refactor

--- functions/twilioHandler.js
+++ functions/twilioHandler.js
@@ -9,27 +9,6 @@ const { urlencoded } = require('body-parser');
 const firebase = require('firebase');
 
 //firebase connection
-
-//fb snapshot
-// database.ref().on("value", function(snapshot) {
-
-//     // Then we console.log the value of snapshot
-//   console.log(snapshot.val());
-
-//     // Then we change the html associated with the number.
-//   //$("#click-value").text(snapshot.val().clickCount);
-
-//     // Then update the clickCounter variable with data from the database.
-//   var clickCounter = snapshot.val().clickCount;
-
-//     // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-//     }, function(errorObject) {
-
-//     // In case of error this will print the error
-//   console.log("The read failed: " + errorObject.code);
-// });
-//
-
 var config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -38,9 +17,27 @@ var config = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
-
 firebase.initializeApp(config);
 var database = firebase.database();
+//fb snapshot
+database.ref().on("value", function(snapshot) {
+
+    // Then we console.log the value of snapshot
+  console.log(snapshot.val());
+
+    // Then we change the html associated with the number.
+  //$("#click-value").text(snapshot.val().clickCount);
+
+    // Then update the clickCounter variable with data from the database.
+  var clickCounter = snapshot.val().clickCount;
+
+    // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
+    }, function(errorObject) {
+
+    // In case of error this will print the error
+  console.log("The read failed: " + errorObject.code);
+});
+//
 
 
 // Set up our express web application
@@ -53,18 +50,15 @@ router.post('/sms', (request, response) => {
   console.log(
     `Incoming message from ${request.body.From}: ${request.body.Body}`
   );
-
   
 
-
+  database.ref().set({
+    clickCount: request.body.Body
+  });
   response.send(`
     <Response>
     </Response>
-  `)
-  .then(  database.ref().set({
-    clickCount: request.body.Body
-  }))
-  ;
+  `);
  
 });
 
