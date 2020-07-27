307ed5eb5fc4505ed73b8b590cbbe2465cebc59d
s3rj <astralninja@gmail.com>
2020-07-27 16:17:54 -0700

    serverless refactor

--- functions/twilioHandler.js
+++ functions/twilioHandler.js
@@ -30,6 +30,17 @@ const firebase = require('firebase');
 // });
 //
 
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
 
 // Set up our express web application
 //const PORT = 8767;
@@ -41,16 +52,7 @@ router.post('/sms', (request, response) => {
   console.log(
     `Incoming message from ${request.body.From}: ${request.body.Body}`
   );
-  var config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+
   
 
   database.ref().set({
